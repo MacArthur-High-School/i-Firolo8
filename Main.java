89a64aae45ce34ad28e0110fdde1b266f2290516
Firolo8 <145720539+Firolo8@users.noreply.github.com>
2023-09-21 16:47:12 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = (int) (3 * Math.random() + 5);
+        System.out.println(x);
+        }
+    }
